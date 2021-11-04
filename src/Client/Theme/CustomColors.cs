00d10d4b59797da931a650740413d1d144d00291
trihugger <wowjeffsilva@gmail.com>
2021-11-03 20:05:52 -0400

    First Correction to add a fancy letter intro to the app.

--- src/Client/Theme/CustomColors.cs
+++ src/Client/Theme/CustomColors.cs
@@ -4,14 +4,14 @@
     {
         public static class Light
         {
-            public const string Primary = "#3eaf7c";
+            public const string Primary = "#2d5ceb"; // #3eaf7c
             public const string Background = "#FFF";
             public const string AppbarBackground = "#FFF";
             public const string AppbarText = "#6e6e6e";
         }
         public static class Dark
         {
-            public const string Primary = "#3eaf7c";
+            public const string Primary = "#2d5ceb"; // #3eaf7c
             public const string Background = "#1b1f22";
             public const string AppbarBackground = "#1b1f22";
             public const string DrawerBackground = "#121212";
